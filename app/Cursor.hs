67a59b6ff8e32fe75cffa1c44f3347a033f4c70d
Boarders <callan.mcgill@gmail.com>
2020-07-21 17:40:49 -0400

    Add basic functionality for line cursor

---
+++ app/Cursor.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Cursor where
+
+import Data.Text as Text
+import Brick.Widgets.Core
+
+data DocCursor =
+  DocCursor
+  { docBefore :: Text
+  , docCurr   :: LineCursor
+  , docAfter  :: Text
+  }
+  deriving (Eq, Show)
+
+data LineCursor =
+  LineCursor
+  { lineBefore :: Text
+  , lineAfter  :: Text
+  }
+  deriving (Eq, Show)
+
+lcTextWidth :: LineCursor -> Int
+lcTextWidth LineCursor{..} = textWidth lineBefore
+
+
+lcToText :: LineCursor -> Text
+lcToText LineCursor{..} = lineBefore <> lineAfter
+
+lcInsert :: Char -> LineCursor -> LineCursor
+lcInsert c LineCursor{..} = LineCursor before' lineAfter
+  where
+    before' = lineBefore `snoc` c
+
+
+lcPrev :: LineCursor -> LineCursor
+lcPrev LineCursor{..} = LineCursor before after
+  where
+    (before, after) =
+      case unsnoc lineBefore of
+        Just (bf, c) -> (bf, cons c lineAfter)
+        Nothing      -> (mempty, lineAfter)
+
+lcNext :: LineCursor -> LineCursor
+lcNext LineCursor{..} = LineCursor before after
+  where
+    (before, after) =
+      case uncons lineAfter of
+        Just (c, af) -> (snoc lineBefore c, af)
+        Nothing      -> (lineBefore, lineAfter)        
+
+lcDeleteBack :: LineCursor -> LineCursor
+lcDeleteBack LineCursor{..} = LineCursor before lineAfter
+  where
+    before = Text.init lineBefore
+
+
+lcDeleteForward :: LineCursor -> LineCursor
+lcDeleteForward LineCursor{..} = LineCursor lineBefore after
+  where
+    after = Text.tail after
+
+
+lcTextStart :: LineCursor -> LineCursor
+lcTextStart LineCursor{..} = LineCursor before mempty
+  where
+    before = lineBefore <> lineAfter
+
+lcTextEnd :: LineCursor -> LineCursor
+lcTextEnd LineCursor{..} = LineCursor mempty after
+  where
+    after = lineBefore <> lineAfter
+
+lcDeleteAll :: LineCursor -> LineCursor
+lcDeleteAll _ = LineCursor mempty mempty
