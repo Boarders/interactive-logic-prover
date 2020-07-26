4439a5ced21431c4adb45b43e79eba7eb3b5044f
Boarders <callan.mcgill@gmail.com>
2020-07-26 11:17:45 -0400

    Add function to handle whitespace nicely

--- app/Highlight.hs
+++ app/Highlight.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE BangPatterns      #-}
 
 module Highlight where
 
@@ -9,14 +10,19 @@ import Brick
 import Brick.Widgets.Core
 import Graphics.Vty.Attributes as Vty
 
+import Data.Text.Internal
+import Data.Text.Unsafe
+import Data.Char
 
-data HighlightType  = HTVar | HTKeyword
+
+data HighlightType  = HTVar | HTKeyword | None
 
 hlTypeToAttrName :: HighlightType -> AttrName
 hlTypeToAttrName =
   \case
     HTVar     -> "var"
     HTKeyword -> "keyword"
+    None      -> ""
 
 type HighlightText = [(Text, HighlightType)]
 
@@ -29,18 +35,20 @@ renderText = htRender . textToHighlightText
 
 -- to do: handle new lines by doing lines and unlines outside
 textToHighlightText :: Text -> HighlightText
-textToHighlightText = fmap highlight . Text.words
+textToHighlightText = fmap (either handleWS highlight) . wsSplit
   where
     highlight :: Text -> (Text, HighlightType)
     highlight t =
       case t `elem` keywords of
-        True -> (t `snoc` ' ', HTKeyword)
-        _    -> (t `snoc` ' ', HTVar)
+        True -> (t , HTKeyword)
+        _    -> (t , HTVar)
+
+    handleWS :: Text -> (Text, HighlightType)
+    handleWS t = (t, None)
 
 htRender :: HighlightText -> Widget Text
 htRender =
     Prelude.foldr (\(t,ht) acc -> (renderWord (t, ht)) <+> acc) emptyWidget
-
   where
     renderWord :: (Text, HighlightType) -> Widget Text
     renderWord (t, ht) =
@@ -48,4 +56,19 @@ htRender =
       $ txt t
       
     
-
+wsSplit :: Text -> [Either Text Text]
+wsSplit t@(Text arr off len)
+  | Text.null t           = []
+  | isSpace (Text.head t) = loop 0 0 True
+  | otherwise             = loop 0 0 False
+  where
+    loop !start !n !wasSpace
+        | n >= len = if wasSpace
+                     then [Left  $ Text arr (start+off) (n-start)]
+                     else [Right $ Text arr (start+off) (n-start)]
+        | isSpace c && wasSpace = loop start (n + d) True
+        | wasSpace  = Left  (Text arr (start+off) (n-start)) : loop n (n+d) False
+        | isSpace c = Right (Text arr (start+off) (n-start)) : loop n (n+d) True
+        | otherwise = loop start (n+d) False
+        where Iter c d = iter t n
+              
